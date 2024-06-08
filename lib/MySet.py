02d7097ea3f9652fd470969fdf703988ec309601
CHARITY-WACHIRA <kouwywachira@gmail.com>
2024-06-08 04:56:47 +0300

    completed assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,31 @@
 class MySet:
+    def __init__(self ,enumerable = []):
+        self.dictionary ={}
+        for value in enumerable:
+            self.dictionary[value]=True
 
-    pass
+    def has(self,value):
+        return value in self.dictionary
+    
+    def add(self,value):
+        self.dictionary[value]=True
+        return self
+    
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+
+    def __str__(self):
+        elements = sorted(self.dictionary.keys())
+        elements_str = ', '.join(str(elem) for elem in elements)
+        return f'MySet: {{{elements_str}}}'
+    
+
+
+    
